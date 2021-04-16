218aede5c5221aafe3623f8a0bc25706b7771eb9
Ruaidhri Carey <rjcarey@perse.co.uk>
2021-04-16 09:29:27 +0000

    turn and winner check implemented

--- Game.py
+++ Game.py
@@ -1,18 +1,27 @@
 
 class Game:
-
+    
+    EMPTY = " "
+    P1 = "o"
+    P2 = "x"
+    
     def __init__(self):
-        pass
-
+        self.__board = [[Game.EMPTY]*3 for _ in range(3)]
+        self.__player = Game.P1 
+        
     def __repr__(self):
         pass
 
     def play(self,row,col):
-        pass
+        self.__board[row][column] = self.__player
+        self.__player = Game.P1 if self.__player == Game.P2 else Game.P2
     
     @property
     def winner(self):
-        pass
+        if self.__board[0][0] == Game.P1 and self.__board[0][1] == Game.P1 and self.__board[0][2] == Game.P1:
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
     pass
