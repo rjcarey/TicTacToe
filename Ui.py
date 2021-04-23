b16ed637350827c7bb61b5bff25c88c1d03aee6b
Ruaidhri Carey <rjcarey@perse.co.uk>
2021-04-23 08:50:38 +0000

    Ob 10, 11, 12 and 13 complete, basic gui game finished

--- Ui.py
+++ Ui.py
@@ -1,6 +1,7 @@
 from Game import Game, GameError
 from abc import ABC, abstractmethod
-from tkinter import Button, Tk, Frame, X
+from tkinter import Button, Tk, Frame, X, Toplevel, StringVar
+from itertools import product
 
 class Ui(ABC):
 
@@ -36,8 +37,33 @@ class Gui(Ui):
         pass
     
     def _play_callback(self):
-        pass
+        self.__game = Game()
+        game_win = Toplevel(self.__root)
+        game_win.title("Game")
+        frame = Frame(game_win)
+        frame.grid(row=0, column=0)
+        
+        Button(game_win, text="Dismis", command=game_win.destroy).grid(row=1, column=0)
+        
+        # only allows for one game at a time
+        self.__buttons = [[None]*3 for _ in range(3)]
+        
+        for row, col in product(range(3), range(3)):
+            b = StringVar()
+            b.set(self.__game.at(row+1, col+1))
+            
+            cmd = lambda r=row, c=col: self.__play_and_refresh(r,c)
+            
+            Button(frame,textvariable=b,command=cmd).grid(row=row, column=col)
+            self.__buttons[row][col] = b
     
+    def __play_and_refresh(self, row, col):
+        self.__game.play(row+1, col+1)
+        
+        for row, col in product(range(3), range(3)):
+            text = self.__game.at(row+1, col+1)
+            self.__buttons[row][col].set(text)
+        
     def _quit_callback(self):
         self.__root.quit()
     
